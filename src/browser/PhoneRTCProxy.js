5227caef23492914bd956fdfd5f15f503a571f8e
sfrinx <simone.caminati@exaltech.it>
2016-09-19 17:57:44 +0200

    Update PhoneRTCProxy.js

--- src/browser/PhoneRTCProxy.js
+++ src/browser/PhoneRTCProxy.js
@@ -3,7 +3,7 @@ var IceCandidate = window.RTCIceCandidate;
 var SessionDescription = window.RTCSessionDescription;
 var MediaStream = window.MediaStream || window.webkitMediaStream;
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mediaDevices.getUserMedia;
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 
 var localStreams = [];
 var localVideoTrack, localAudioTrack;
