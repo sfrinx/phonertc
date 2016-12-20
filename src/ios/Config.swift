67d446ce7c9f1dfafe9d57fd43ac7677695b63f0
sfrinx <simone.caminati@exaltech.it>
2016-12-20 10:48:37 +0100

    Update Config.swift

--- src/ios/Config.swift
+++ src/ios/Config.swift
@@ -5,17 +5,17 @@ class SessionConfig {
     var turn: TurnConfig
     var streams: StreamsConfig
     
-    init(data: AnyObject) {
+    init(data: Any) {
         self.isInitiator = data.objectForKey("isInitiator") as! Bool
         
-        let turnObject: AnyObject = data.objectForKey("turn")!
+        let turnObject: Any = data.objectForKey("turn")!
         self.turn = TurnConfig(
             host: turnObject.objectForKey("host") as! String,
             username: turnObject.objectForKey("username") as! String,
             password: turnObject.objectForKey("password") as! String
         )
         
-        let streamsObject: AnyObject = data.objectForKey("streams")!
+        let streamsObject: Any = data.objectForKey("streams")!
         self.streams = StreamsConfig(
             audio: streamsObject.objectForKey("audio") as! Bool,
             video: streamsObject.objectForKey("video") as! Bool
@@ -38,9 +38,9 @@ class VideoConfig {
     var container: VideoLayoutParams
     var local: VideoLayoutParams?
     
-    init(data: AnyObject) {
-        let containerParams: AnyObject = data.objectForKey("containerParams")!
-        let localParams: AnyObject? = data.objectForKey("local")
+    init(data: Any) {
+        let containerParams: Any = data.objectForKey("containerParams")!
+        let localParams: Any? = data.objectForKey("local")
         
         self.container = VideoLayoutParams(data: containerParams)
         
@@ -60,12 +60,12 @@ class VideoLayoutParams {
         self.height = height
     }
     
-    init(data: AnyObject) {
-        let position: [AnyObject] = data.objectForKey("position")! as! [AnyObject]
+    init(data: Any) {
+        let position: [Any] = data.objectForKey("position")! as! [Any]
         self.x = position[0] as! Int
         self.y = position[1] as! Int
         
-        let size: [AnyObject] = data.objectForKey("size")! as! [AnyObject]
+        let size: [Any] = data.objectForKey("size")! as! [Any]
         self.width = size[0] as! Int
         self.height = size[1] as! Int
     }
